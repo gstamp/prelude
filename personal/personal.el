705089f8cb28665bd8304f397ce697ddfb3e88c4
Glen Stampoultzis <gstamp@gmail.com>
2013-09-13 13:52:47 +1000

    Added initial prelude config.

---
+++ personal/personal.el
@@ -0,0 +1,418 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Some sane defaults
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Zernburn is not awesome, soothe is not bad
+(disable-theme 'zenburn)
+(prelude-require-packages '(soothe-theme))
+(load-theme 'soothe)
+
+;; I like arrow keys turned on
+(setq prelude-guru nil)
+
+;; No thanks to flyspell mode being on by default
+(setq prelude-flyspell nil)
+
+;; JS indent levels
+(setq js-indent-level 2)
+(setq js2-basic-offset 2)
+
+;; By default, Emacs inserts tabs in place of multiple spaces when it
+;; formats a region. We want spaces.
+(setq-default intent-tabs-mode nil)
+
+;; Setup flx (https://github.com/lewang/flx)
+(require 'flx-ido)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-use-faces nil)
+
+;; Indenting defaults
+(setq nxml-child-indent 4)
+(setq lua-indent-level 4)
+
+;; Allow emacs to access the x clipboard
+(setq x-select-enable-clipboard t)
+
+;; Set the GC limit to 40MB to avoid over GCage.
+(setq gc-cons-threshold 40000000)
+
+;;  The variable vc-follow-symlinks controls what Emacs does if you
+;;  try to visit a symbolic link pointing to a version-controlled
+;;  file.
+(setq vc-follow-symlinks t)
+
+;; flash instead of ding
+(setq visible-bell t)
+
+;; enable fuzzy matching
+(setq ido-enable-flex-matching t)
+
+;; Put backups in ~/.emacsbackup/ and control how many versions are kept
+(setq backup-directory-alist
+      (list (cons ".*" (expand-file-name "~/.emacsbackup/"))))
+(setq version-control t)                ; backup multiple versions
+(setq delete-old-versions t)            ; delete the older ones
+(setq kept-new-versions 10)             ; keep x new ones
+(setq kept-old-versions 3)              ; keep x old ones
+
+;; TODO: Not sure this is actually doing anything
+;; Sets up whitepsace.
+;;  trailing - no trailing whitespace and end of line
+;;  lines - lines whose have columns beyond ‘whitespace-line-column’
+;;          are highlighted via faces.
+;;  tabs - TABs are visualized via faces.
+(setq whitespace-style '(trailing lines tabs)
+      whitespace-line-column 80)        ; no trailing space or tabs
+
+;; gerkin config
+(setq feature-default-language "en")
+
+;; Display/redraw defaults
+(setq set-cursor-type 'box)
+(set-cursor-color "yellow")
+
+;; Can't see a good reason not to enable these commands
+(put 'erase-buffer 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+;; Emacs starter kit turns on flyspell by default but I prefer it off
+(remove-hook 'text-mode-hook 'turn-on-flyspell)
+
+;; Emacs starter kit also turns on idle-highlight-mode.  Not a fan.
+(remove-hook 'prog-mode-hook 'esk-turn-on-idle-highlight-mode)
+
+;; Redraw more frequently
+(setq redisplay-dont-pause 't)
+
+;; Add some extra snippets
+;;GJS: (require 'yasnippet)
+;;GJS: (setq yas/snippet-dirs
+;;GJS:       '("~/.emacs.d/snippets"))
+;;GJS: (yas/global-mode 1)
+
+;; Stop ERC telling me about all those people joining/quiting.
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
+
+;; Increase find file in project file limit
+(setq ffip-limit 3500)
+
+(setq feature-cucumber-command "bundle exec cucumber {options} {feature}")
+
+(when (equal system-type 'darwin)
+  (progn
+    ;; map meta to the command key on mac
+    (setq mac-option-key-is-meta nil)
+    (setq mac-left-command-key-is-meta t)
+    (setq mac-command-modifier 'meta)
+    (setq mac-option-modifier nil)
+
+    ;; I like the right option key to be control because there's no
+    ;; right control key on the mac
+    (setq mac-right-option-modifier 'control)
+
+    ;; sets fn-delete to be right-delete
+    (global-set-key [kp-delete] 'delete-char)
+
+    ;; keybinding to toggle full screen mode
+    (global-set-key (quote [M-f10]) (quote ns-toggle-fullscreen))
+
+    ;; Move to trash when deleting stuff
+    (setq delete-by-moving-to-trash t
+          trash-directory "~/.Trash/emacs")
+
+    (setenv "LANG" "en_AU.UTF-8")
+
+    (defadvice ansi-term (after advise-ansi-term-coding-system)
+      (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+    (ad-activate 'ansi-term)
+    (defadvice multi-term (after advise-multi-term-coding-system)
+      (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+    (ad-activate 'multi-term)
+
+    (prefer-coding-system 'utf-8)
+    (set-default-coding-systems 'utf-8)
+    (set-terminal-coding-system 'utf-8)
+    (set-keyboard-coding-system 'utf-8)
+
+    ;; mac friendly font
+    (if window-system
+        (set-face-attribute 'default nil :font "Monaco-12"))
+    ))
+
+;; Open the buffer list in the same window
+(add-to-list 'same-window-buffer-names "*Buffer List*")
+
+;; Add rainbow delimiters to all programming modes
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+;; Alternatively it can be hooked up to specific modes:
+;; (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+;; ..or globally..
+;; (global-rainbow-delimiters-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Setup: Buffer Management
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup: Ack and a Half
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(prelude-ensure-module-deps '(ack-and-a-half))
+
+(eval-after-load 'ack-and-a-half
+  '(progn
+     (setq ack-and-a-half-root-directory-functions 'ack-and-a-half-guess-project-root)))
+
+(global-set-key [f2] 'ack-and-a-half)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Setup: Editor helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun align-to-equals (begin end)
+  "Align region to equal signs"
+  (interactive "r")
+  (align-regexp begin end "\\(\\s-*\\)=" 1 1 ))
+
+(defun finder ()
+  "Open the current working directory in finder."
+  (interactive)
+  (shell-command (concat "open " (shell-quote-argument default-directory))))
+
+(defun join-with-next-line ()
+  "join with next line"
+  (interactive)
+  (next-line)
+  (delete-indentation))
+
+(global-set-key [(control shift j)] 'join-with-next-line)
+
+(defun iwb ()
+  "indent whole buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+;; TODO: setup a keybinding for this
+
+
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
+
+(global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
+
+;; Make open-line work more like VI (bound to ctrl-o)
+(defadvice open-line (before new-open-line activate)
+  (end-of-visible-line))
+(defadvice open-line (after after-open-line activate)
+  (forward-line 1)
+  (indent-according-to-mode))
+
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive)
+  (if (not (string-equal major-mode "org-mode"))
+      (let ((oldpos (point)))
+        (back-to-indentation)
+        (and (= oldpos (point))
+             (beginning-of-line)))
+    (move-beginning-of-line nil)))
+
+(global-set-key [home] 'smart-beginning-of-line)
+
+;; this will indent the yanked region automatically in the provided
+;; modes
+(defadvice yank (after indent-region activate)
+  (if (member major-mode '(emacs-lisp-mode scheme-mode
+                                           lisp-mode
+                                           clojure-mode
+                                           ruby-mode
+                                           c-mode
+                                           c++-mode
+                                           objc-mode
+                                           LaTeX-mode
+                                           TeX-mode))
+      (indent-region (region-beginning) (region-end) nil)))
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy the current line."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (progn
+       (message "Current line is copied.")
+       (list (line-beginning-position) (line-beginning-position 2)) ) ) ))
+
+(defadvice kill-region (before slick-copy activate compile)
+  "When called interactively with no active region, cut the current line."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (progn
+       (list (line-beginning-position) (line-beginning-position 2)) ) ) ))
+
+(defun mark-line-or-next ()
+  "Marks the current line or extends the mark if there is no current selection"
+  (interactive)
+  (if mark-active
+      (forward-line)
+    (progn
+      (beginning-of-line)
+      (push-mark (point))
+      (end-of-line)
+      (forward-char)
+      (activate-mark))))
+
+(global-set-key (kbd "C-;") 'mark-line-or-next)
+
+(defun smart-kill-whole-line (&optional arg)
+  "A simple wrapper around `kill-whole-line' that respects indentation."
+  (interactive "P")
+  (kill-whole-line arg)
+  (back-to-indentation))
+
+(global-set-key [remap kill-whole-line] 'smart-kill-whole-line)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Setup: Helper Functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Stops the mini buffer when switching back to emacs with mouse
+(defun stop-using-minibuffer ()
+  "kill the minibuffer"
+  (when (and (>= (recursion-depth) 1) (active-minibuffer-window))
+    (abort-recursive-edit)))
+(add-hook 'mouse-leave-buffer-hook 'stop-using-minibuffer)
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+
+(defun ert-run ()
+  "Evaluate the current buffer and run ert testing framework"
+  (interactive)
+
+  (eval-buffer)
+  (ert 't)
+  )
+(global-set-key [f5] 'ert-run)
+
+(defun url-decode-region (start end)
+  "Replace a region with the same contents, only URL decoded."
+  (interactive "r")
+  (let ((text (url-unhex-string (buffer-substring start end))))
+    (delete-region start end)
+    (insert text)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Setup: General Keybindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(global-set-key [f1] 'ido-switch-buffer)
+(global-set-key [f3] 'kmacro-start-macro-or-insert-counter)
+(global-set-key [f4] 'kmacro-end-or-call-macro)
+(global-set-key [f8] 'find-file-at-point)
+(global-set-key [f10] 'multi-term)
+(global-set-key [f11] 'ido-kill-buffer)
+
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [(control f4)] 'kill-this-buffer)
+(autoload 'linum-mode "linum" "toggle line numbers on/off" t)
+(global-set-key [(control f5)] 'linum-mode)
+;; Turn on the menu bar for exploring new modes
+(global-set-key (kbd "C-<f10>") 'menu-bar-mode)
+
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+
+;; Selects the definition which encloses the point
+(global-set-key (kbd "C-M-h") 'mark-defun)
+
+(global-set-key "\r" 'newline-and-indent)
+
+(global-set-key (kbd "<S-return>") 'open-line)
+(global-set-key (kbd "C-S-o") '"\C-p\C-o") ; open line above
+
+;; Move more quickly
+(global-set-key (kbd "C-S-n") (lambda () (interactive) (ignore-errors (next-line 5))))
+(global-set-key (kbd "C-S-p") (lambda () (interactive) (ignore-errors (previous-line 5))))
+(global-set-key (kbd "C-S-f") (lambda () (interactive) (ignore-errors (forward-char 5))))
+(global-set-key (kbd "C-S-b") (lambda () (interactive) (ignore-errors (backward-char 5))))
+
+;; This adds an extra keybinding to interactive search (C-s) that runs
+;; occur on the current search string/regexp, immediately showing all
+;; hits in the entire buffer. I use it all the time now.
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda ()
+    (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp isearch-string
+               (regexp-quote isearch-string))))))
+
+
+
