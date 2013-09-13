acf67b467031a984bf3cfbee6f98a4ade131191e
Glen Stampoultzis <gstamp@gmail.com>
2013-09-13 18:18:40 +1000

    Setup ido and display it vertically

--- personal/personal.el
+++ personal/personal.el
@@ -505,5 +505,26 @@ If point was already at that position, move point to beginning of line."
       (occur (if isearch-regexp isearch-string
                (regexp-quote isearch-string))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Setup: Ido
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; ido defaults
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2)
 
+;; Make ido open file list vertically
+(setq ido-max-prospects 60)
+(setq ido-max-file-prompt-width 0.8)
+(setq ido-max-window-height 30)
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+
+(setq ffip-full-paths 't) ; Not really part of ido but works well with
+                                        ; vertically displayed lists.
