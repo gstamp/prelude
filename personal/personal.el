49a6c9b52cb2fab9802ae914c6e5a838b40e440a
Glen Stampoultzis <gstamp@gmail.com>
2013-10-08 08:28:32 +1100

    Swap isearch regex and normal isearch keybindings

--- personal/personal.el
+++ personal/personal.el
@@ -516,6 +516,13 @@ If point was already at that position, move point to beginning of line."
       (occur (if isearch-regexp isearch-string
                (regexp-quote isearch-string))))))
 
+;; Use regex searches by default.
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "\C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Setup: Ido
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
