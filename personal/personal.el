9fde5112073f915361d590df5d163ebae5670903
Glen Stampoultzis <gstamp@gmail.com>
2013-10-18 08:58:53 +1100

    Allow the user of occur during an incremental search
    Press C-o while searching

--- personal/personal.el
+++ personal/personal.el
@@ -455,6 +455,10 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda nil (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Setup: Ido
