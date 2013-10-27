387b76e90fee92b9a098f4db5004a5ef0b51a48a
Glen Stampoultzis <gstamp@gmail.com>
2013-10-28 10:46:28 +1100

    Add URL humanify function

--- personal/personal.el
+++ personal/personal.el
@@ -389,6 +389,26 @@
   )
 (global-set-key [f5] 'ert-run)
 
+(defun url-humanify ()
+  "Take the URL at point and make it human readable."
+  (interactive)
+  (let* ((area (bounds-of-thing-at-point 'url))
+         (num-params  (count-occurances-in-region "&" (car area) (cdr area)))
+         (i 0))
+    (beginning-of-thing 'url)
+    (when (search-forward "?" (cdr area) t nil)
+      (insert "\n  ")
+      (while (< i num-params)
+        (search-forward "&" nil t nil)
+        (insert "\n  ")
+        (save-excursion
+          (previous-line)
+          (beginning-of-line)
+          (let ((start (search-forward "="))
+                (end (search-forward "&")))
+            (url-decode-region start end)))
+        (setq i (+ i 1))))))
+
 (defun url-decode-region (start end)
   "Replace a region with the same contents, only URL decoded."
   (interactive "r")
@@ -396,6 +416,13 @@
     (delete-region start end)
     (insert text)))
 
+(defun count-occurances-in-region (needle start end)
+  (save-excursion
+    (let ((found 0))
+      (goto-char start)
+      (while (search-forward needle end t nil)
+        (setq found (+ found 1)))
+      found)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Setup: Git Gutter
