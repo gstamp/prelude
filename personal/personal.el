92c892cb3bc31f650f9fb995abc22cfce0bfdfbe
Glen Stampoultzis <glen.stampoultzis@envato.com>
2013-12-05 16:25:16 +1100

    Extra alignment functions

--- personal/personal.el
+++ personal/personal.el
@@ -309,6 +309,24 @@
   (interactive "r")
   (align-regexp begin end "\\(\\s-*\\)=" 1 1 ))
 
+(defun align-to-colon (begin end)
+  "Align region to colon (:) signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) ":") 1 1 ))
+
+(defun align-to-comma (begin end)
+  "Align region to comma signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx "," (group (zero-or-more (syntax whitespace))) ) 1 1 ))
+
+(defun align-to-hash (begin end)
+  "Align region to hash ( => ) signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) "=>") 1 1 ))
+
 (defun finder ()
   "Open the current working directory in finder."
   (interactive)
