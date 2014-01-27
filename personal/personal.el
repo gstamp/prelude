6f5e34848d53958b377163f8f0d3e56beee0bf1c
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-01-28 10:50:52 +1100

    Use ido-vertical-mode instead of custom changes

--- personal/personal.el
+++ personal/personal.el
@@ -525,13 +525,10 @@
       ido-use-filename-at-point 'guess
       ido-use-virtual-buffers t)
 
-;; Make ido open file list vertically
-(setq ido-max-prospects 60)
-(setq ido-max-file-prompt-width 0.8)
-(setq ido-max-window-height 30)
-(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
-(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
-(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(prelude-require-packages '(ido-vertical-mode))
+
+(ido-vertical-mode +1)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Setup: Golden ratio plugin
