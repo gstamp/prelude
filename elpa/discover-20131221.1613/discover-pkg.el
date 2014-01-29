2993bb176785c743de1984027a0636cfdb7fc6f4
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-01-29 11:11:33 +1100

    Update all plugins

--- elpa/discover-20131221.1613/discover-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "discover" "20131221.1613" "discover more of Emacs" (quote ((makey "0.2"))))
--- elpa/makey-20131221.1612/makey-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; makey-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("makey-pkg.el" "makey.el") (21176 45354
-;;;;;;  477299 0))
-
-;;;***
-
-(provide 'makey-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; makey-autoloads.el ends here
