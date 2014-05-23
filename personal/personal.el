1c74234231af44b335e1923c2770267f09f0c489
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-05-23 10:57:30 +1000

    Improve terminal encoding on OS/X

--- personal/personal.el
+++ personal/personal.el
@@ -134,6 +134,9 @@
       (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
     (ad-activate 'multi-term)
 
+    ;; Fixes multiterm problems I'm having
+    (setq locale-coding-system 'utf-8)
+    (set-selection-coding-system 'utf-8)
     (prefer-coding-system 'utf-8)
     (set-default-coding-systems 'utf-8)
     (set-terminal-coding-system 'utf-8)
