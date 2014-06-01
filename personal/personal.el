e6e2666f0e8ca672c6ec958521e88601dadc5f94
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-06-02 08:18:44 +1000

    Change zap-to-char to not include zapped character itself.

--- personal/personal.el
+++ personal/personal.el
@@ -353,6 +353,13 @@
   (align-regexp begin end
                 (rx (group (zero-or-more (syntax whitespace))) "=>") 1 1 ))
 
+(defadvice zap-to-char (after my-zap-to-char-advice (arg char) activate)
+  "Kill up to the ARG'th occurence of CHAR, and leave CHAR. If
+  you are deleting forward, the CHAR is replaced and the point is
+  put before CHAR"
+  (insert char)
+  (if (< 0 arg) (forward-char -1)))
+
 (defun finder ()
   "Open the current working directory in finder."
   (interactive)
