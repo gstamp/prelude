99ee3667063cba303d987ccdc31f96c69681cc04
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-07-18 18:50:26 +1000

    Remove MC keybindings from autoload

--- personal/personal.el
+++ personal/personal.el
@@ -720,13 +720,11 @@
 
 (prelude-require-package 'multiple-cursors)
 
-(eval-after-load 'multiple-cursors-autoloads
-  '(progn
-     (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-     (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-     (global-set-key (kbd "C-*") 'mc/mark-all-dwim)
-     (global-unset-key (kbd "M-<down-mouse-1>"))
-     (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)))
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-*") 'mc/mark-all-dwim)
+(global-unset-key (kbd "M-<down-mouse-1>"))
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
