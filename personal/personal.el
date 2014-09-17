4936b14c0f5c9d4f69efde3f2084462303dff35a
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-09-18 09:58:02 +1000

    Remove GC settings from personal.el (they're in init anyhoo)

--- personal/personal.el
+++ personal/personal.el
@@ -43,9 +43,6 @@
 ;; Allow emacs to access the x clipboard
 (setq x-select-enable-clipboard t)
 
-;; Set the GC limit to 80MB to avoid over GCage.
-(setq gc-cons-threshold 80000000)
-
 ;;  The variable vc-follow-symlinks controls what Emacs does if you
 ;;  try to visit a symbolic link pointing to a version-controlled
 ;;  file.
