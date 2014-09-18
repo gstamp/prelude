84c590f4da6ca27286e02f688216c419fba480ee
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-09-19 08:14:50 +1000

    Flycheck update

--- elpa/flycheck-20140914.133/flycheck-autoloads.el
+++ elpa/flycheck-20140914.133/flycheck-autoloads.el
@@ -1,15 +1,10 @@
 ;;; flycheck-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (flycheck-define-error-level flycheck-info flycheck-def-option-var
-;;;;;;  flycheck-def-config-file-var global-flycheck-mode flycheck-mode)
-;;;;;;  "flycheck" "flycheck.el" (21264 3130 0 0))
+;;;### (autoloads nil "flycheck" "flycheck.el" (21530 32362 0 0))
 ;;; Generated autoloads from flycheck.el
-
-(defconst flycheck-mode-line-lighter " FlyC" "\
-The standard lighter for flycheck mode.")
 
 (autoload 'flycheck-mode "flycheck" "\
 Minor mode for on-the-fly syntax checking.
@@ -55,38 +50,38 @@ See `flycheck-mode' for more information on Flycheck mode.
 (autoload 'flycheck-def-config-file-var "flycheck" "\
 Define SYMBOL as config file variable for CHECKER, with default FILE-NAME.
 
-SYMBOL is declared as customizable variable (see `defcustom`)
-providing a configuration file for CHECKER.  The CHECKER argument
-is used for documentation purposes only.  If given use FILE-NAME
-as initial value.
+SYMBOL is declared as customizable, buffer-local variable using
+`defcustom', to provide a configuration file for the given syntax
+CHECKER.  CUSTOM-ARGS are forwarded to `defcustom'.
 
-The variable is declared with `defcustom', and declared
-buffer-local.  CUSTOM-ARGS are forwarded to `defcustom'
+FILE-NAME is the initial value of the new variable.  If omitted,
+the default value is nil.
 
-Use this together with the `config-file' cell in syntax checker
-commands.
+Use this together with the `config-file' form in the `:command'
+argument to `flycheck-define-checker'.
 
 \(fn SYMBOL CHECKER &optional FILE-NAME &rest CUSTOM-ARGS)" nil t)
 
-(put 'flycheck-def-config-file-var 'lisp-indent-function '3)
+(function-put 'flycheck-def-config-file-var 'lisp-indent-function '3)
 
 (autoload 'flycheck-def-option-var "flycheck" "\
 Define SYMBOL as option variable with INIT-VALUE for CHECKER.
 
-INIT-VALUE is the initial value for the new variable.  DOCSTRING
-is its docstring.
+SYMBOL is declared as customizable variable, buffer-local
+variable using `defcustom', to provide an option for the given
+syntax CHECKER.  INIT-VALUE is the initial value of the variable,
+and DOCSTRING is its docstring.  CUSTOM-ARGS are forwarded to
+`defcustom'.
 
-The variable is declared with `defcustom', and declared
-buffer-local.  CUSTOM-ARGS are forwarded to `defcustom'.
-
-Use this together with the `option' cell in syntax checker
-commands.
+Use this together with the `option', `option-list' and
+`option-flag' forms in the `:command' argument to
+`flycheck-define-checker'.
 
 \(fn SYMBOL INIT-VALUE CHECKER DOCSTRING &rest CUSTOM-ARGS)" nil t)
 
-(put 'flycheck-def-option-var 'lisp-indent-function '3)
+(function-put 'flycheck-def-option-var 'lisp-indent-function '3)
 
-(put 'flycheck-def-option-var 'doc-string-elt '4)
+(function-put 'flycheck-def-option-var 'doc-string-elt '4)
 
 (autoload 'flycheck-info "flycheck" "\
 Open the Flycheck manual.
@@ -98,16 +93,32 @@ Define a new error LEVEL with PROPERTIES.
 
 The following PROPERTIES constitute an error level:
 
+`:severity SEVERITY'
+     A number denoting the severity of this level.  The higher
+     the number, the more severe is this level compared to other
+     levels.  Defaults to 0.
+
+     The severity is used by `flycheck-error-level-<' to
+     determine the ordering of errors according to their levels.
+
 `:overlay-category CATEGORY'
      A symbol denoting the overlay category to use for error
      highlight overlays for this level.  See Info
-     node `(elisp)Overlay properties' for more information about
+     node `(elisp)Overlay Properties' for more information about
      overlay categories.
+
+     A category for an error level overlay should at least define
+     the `face' property, for error highlighting.  Other useful
+     properties for error level categories are `priority' to
+     influence the stacking of multiple error level overlays, and
+     `help-echo' to define a default error messages for errors
+     without messages.
 
 `:fringe-bitmap BITMAP'
      A fringe bitmap symbol denoting the bitmap to use for fringe
      indicators for this level.  See Info node `(elisp)Fringe
-     Bitmaps' for more information about fringe bitmaps.
+     Bitmaps' for more information about fringe bitmaps,
+     including a list of built-in fringe bitmaps.
 
 `:fringe-face FACE'
      A face symbol denoting the face to use for fringe indicators
@@ -119,20 +130,18 @@ The following PROPERTIES constitute an error level:
 
 \(fn LEVEL &rest PROPERTIES)" nil nil)
 
-(put 'flycheck-define-error-level 'lisp-indent-function '1)
+(function-put 'flycheck-define-error-level 'lisp-indent-function '1)
 
 ;;;***
 
-;;;### (autoloads nil nil ("flycheck-pkg.el") (21264 3130 464292
+;;;### (autoloads nil nil ("flycheck-pkg.el") (21530 32362 674702
 ;;;;;;  0))
 
 ;;;***
 
-(provide 'flycheck-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; flycheck-autoloads.el ends here
