3ab184f299a8f930879556549c44a518c8de6c1e
Glen Stampoultzis <glen.stampoultzis@envato.com>
2015-04-15 13:18:52 +1000

    Magit update and include magit-filenotify

--- personal/personal.el
+++ personal/personal.el
@@ -1008,7 +1008,7 @@ This command calls the external script 'ruby-to-json.rb'."
 ;;;; Setup: Git/Magit
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(prelude-require-packages '(git-link))
+(prelude-require-packages '(git-link magit-filenotify))
 
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
@@ -1034,6 +1034,13 @@ This command calls the external script 'ruby-to-json.rb'."
             (format "https://github.com/%s/compare/%s"
                     (git-link-remote-dir remote-name)
                     branch))))))
+
+;; Stop magit from warning about auto revert
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+;; Auto refresh magit status
+(add-hook 'magit-status-mode-hook 'magit-filenotify-mode nil nil)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Setup: Dash and point
@@ -1353,7 +1360,6 @@ This function is intended to be used as a value of `ring-bell-function'."
 
 (require 'popwin)
 (popwin-mode)
-
 (setq display-buffer-function 'popwin:display-buffer)
 
 (add-to-list 'popwin:special-display-config '("*rspec-compilation*" :noselect t))
