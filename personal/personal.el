c4378218f0e4a65b52b55f38a02df5b5de547c50
Glen Stampoultzis <glen.stampoultzis@envato.com>
2015-06-23 09:40:27 +1000

    Handy new ediff function

--- personal/personal.el
+++ personal/personal.el
@@ -519,6 +519,28 @@ C-u C-u COMMAND -> Open/switch to a scratch buffer in `emacs-elisp-mode'"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+(defun ediff-marked-pair ()
+  "Run ediff-files on a pair of files marked in dired buffer"
+  (interactive)
+  (let* ((marked-files (dired-get-marked-files nil nil))
+         (other-win (get-window-with-predicate
+                     (lambda (window)
+                       (with-current-buffer (window-buffer window)
+                         (and (not (eq window (selected-window)))
+                              (eq major-mode 'dired-mode))))))
+         (other-marked-files (and other-win
+                                  (with-current-buffer (window-buffer other-win)
+                                    (dired-get-marked-files nil)))))
+    (cond ((= (length marked-files) 2)
+           (ediff-files (nth 0 marked-files)
+                        (nth 1 marked-files)))
+          ((and (= (length marked-files) 1)
+                (= (length other-marked-files) 1))
+           (ediff-files (nth 0 marked-files)
+                        (nth 0 other-marked-files)))
+          (t (error "mark exactly 2 files, at least 1 locally")))))
+
+
 ;; Modifed version of save-some-buffers that saves the buffers but doesn't
 ;; spit out rubbish to the message bage
 (defun simple-save-some-buffers ()
