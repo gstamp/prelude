b4c42a8b5736eb044cdfb11a592418a38cbb243b
Glen Stampoultzis <glen.stampoultzis@envato.com>
2015-06-29 09:57:55 +1000

    Helm adjustments

--- personal/personal.el
+++ personal/personal.el
@@ -830,11 +830,33 @@ C-u C-u COMMAND -> Open/switch to a scratch buffer in `emacs-elisp-mode'"
 ;; max size of filename
 (setq helm-buffer-max-length 30)
 
+;; Some experimental helm defaults
+(setq helm-quick-update                     t ; do not display invisible candidates
+      helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
 ;; Minimal helm - defaults are super chunky
-(setq helm-display-header-line nil)
-(set-face-attribute 'helm-source-header nil :height 0.1)
+;; (setq helm-display-header-line nil)
+;; (set-face-attribute 'helm-source-header nil :height 0.1)
+
+;; Resize automatically
 (helm-autoresize-mode 1)
-(setq helm-split-window-in-side-p nil)
+
+;; Hide minibuffer with helm
+(setq helm-echo-input-in-header-line t)
+(defun helm-hide-minibuffer-maybe ()
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face (let ((bg-color (face-background 'default nil)))
+                              `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+(add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
+
 
 (global-set-key [f1] 'helm-projectile-switch-to-buffer)
 (global-set-key [f2] 'helm-projectile-ag)
