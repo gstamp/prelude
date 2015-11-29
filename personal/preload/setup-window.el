140c8abc07211cd5bd0ad058aa8842ba6096c2b1
Glen Stampoultzis <glen.stampoultzis@envato.com>
2015-11-30 08:14:14 +1100

    Only set font on OS/X

--- personal/preload/setup-window.el
+++ personal/preload/setup-window.el
@@ -1,6 +1,7 @@
 (if window-system
     (progn
-      (set-face-attribute 'default nil :font "Oxygen Mono" :height 140)
+      (when (equal system-type 'darwin)
+        (set-face-attribute 'default nil :font "Oxygen Mono" :height 140))
       (set-frame-size (selected-frame) 124 40)
       )
   )
