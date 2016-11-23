b97416aad6a1ba6a699387af37b4f7a079a850b1
Glen Stampoultzis <glen.stampoultzis@envato.com>
2016-11-24 09:44:40 +1100

    Remove corral

--- personal/personal.el
+++ personal/personal.el
@@ -1578,18 +1578,10 @@ This function is intended to be used as a value of `ring-bell-function'."
 (delete "*vc-diff*" popwin:special-display-config)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Setup: Smart parens & corral
+;;;; Setup: Smart parens
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(prelude-require-packages '(corral smartparens))
-
-(global-set-key (kbd "M-9") 'corral-parentheses-backward)
-(global-set-key (kbd "M-0") 'corral-parentheses-forward)
-(global-set-key (kbd "M-[") 'corral-brackets-backward)
-(global-set-key (kbd "M-]") 'corral-brackets-forward)
-(global-set-key (kbd "M-{") 'corral-braces-backward)
-(global-set-key (kbd "M-}") 'corral-braces-forward)
-(global-set-key (kbd "M-\"") 'corral-double-quotes-backward)
+(prelude-require-packages '(smartparens))
 
 (if (boundp 'zenburn)
     ;; Use a more subtle colour for smartparens overlays
