0ac1e0b1e63f8692f8811d9084aa6e56f3c3b34c
Glen Stampoultzis <glen.stampoultzis@envato.com>
2017-03-18 18:55:09 +1100

    Setup font fallbacks

--- personal/preload/setup-window.el
+++ personal/preload/setup-window.el
@@ -1,7 +1,20 @@
 (if window-system
     (progn
       (when (equal system-type 'darwin)
-        (set-face-attribute 'default nil :font "Oxygen Mono" :height 140))
+        ;; Font fallback
+        (cond
+         ((find-font (font-spec :name "Operator Mono-14:weight=light"))
+          (set-frame-font "Operator Mono-14:weight=light"))
+         ((find-font (font-spec :name "Oxygen Mono-14"))
+          (set-frame-font "Oxygen Mono-13"))
+         ((find-font (font-spec :name "DejaVu Sans Mono"))
+          (set-frame-font "DejaVu Sans Mono-12"))
+         ((find-font (font-spec :name "inconsolata"))
+          (set-frame-font "inconsolata-12"))
+         ((find-font (font-spec :name "Lucida Console"))
+          (set-frame-font "Lucida Console-12"))
+         ((find-font (font-spec :name "courier"))
+          (set-frame-font "courier-12"))))
       (set-frame-size (selected-frame) 124 40)
       )
   )
