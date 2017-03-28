3a53b214a12cf2ed1ecc8f9b8461cd352899e485
Glen Stampoultzis <glen.stampoultzis@envato.com>
2017-03-29 09:20:14 +1100

    Turn off custom fringe for rubocop
    Unfortunately it was causing emacs to crash. :-(

--- personal/personal.el
+++ personal/personal.el
@@ -1634,46 +1634,7 @@ If the error list is visible, hide it.  Otherwise, show it."
 (with-eval-after-load 'flycheck
   (flycheck-pos-tip-mode)
 
-  (define-key flycheck-mode-map (kbd "C-c C-t") 'toggle-flycheck-error-list)
-
-  ;; Custom fringe indicator
-  (when (fboundp 'define-fringe-bitmap)
-    (define-fringe-bitmap 'my-flycheck-fringe-indicator
-      (vector #b00000000
-              #b00000000
-              #b00000000
-              #b00000000
-              #b00000000
-              #b00000000
-              #b00000000
-              #b00011100
-              #b00111110
-              #b00111110
-              #b00111110
-              #b00011100
-              #b00000000
-              #b00000000
-              #b00000000
-              #b00000000
-              #b00000000)))
-
-  (let ((bitmap 'my-flycheck-fringe-indicator))
-    (flycheck-define-error-level 'error
-      :severity 2
-      :overlay-category 'flycheck-error-overlay
-      :fringe-bitmap bitmap
-      :fringe-face 'flycheck-fringe-error)
-    (flycheck-define-error-level 'warning
-      :severity 1
-      :overlay-category 'flycheck-warning-overlay
-      :fringe-bitmap bitmap
-      :fringe-face 'flycheck-fringe-warning)
-    (flycheck-define-error-level 'info
-      :severity 0
-      :overlay-category 'flycheck-info-overlay
-      :fringe-bitmap bitmap
-      :fringe-face 'flycheck-fringe-info))
-  )
+  (define-key flycheck-mode-map (kbd "C-c C-t") 'toggle-flycheck-error-list))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Setup: Shell
